c66a175468b4b859a356d58230e6965c80235179
Talis Tammearu <talis995@gmail.com>
2022-09-07 17:22:09 +0300

    initial

---
+++ AnimLibAssembly.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+
+namespace AnimLib.Resources {
+   public static class AnimLibAssembly {
+      public static readonly Assembly Value = typeof(AnimLibAssembly).Assembly;
+   }
+}
